04f49273878779d3605797e69f8eab1d5c205167
D7CRIS <cris10003@gmail.com>
2024-12-05 19:16:18 +0100

    day 3

--- 2024/days/day3.js
+++ 2024/days/day3.js
@@ -1,9 +1,20 @@
-import { runWithInput } from "../../functions/js"
+import { runWithInput } from "../../function.js";
 
-let numbers = []
+let sum = 0
 
 const clearNumbers = (input) => {
-    input.split()
+    const matches = input.match(/mul\((\d+),(\d+)\)/g);
+    
+    const numbers = matches.map(match => {
+        const [num1, num2] = match.match(/\d+/g);
+        return [parseInt(num1), parseInt(num2)];
+    });
+
+    for (let i = 0; i < numbers.length; ++i){
+        sum += numbers[i][0] * numbers[i][1]
+    }
+
+    return sum
 }
 
-runWithInput("3.txt", clearNumbers)
+runWithInput("3.txt", clearNumbers);
